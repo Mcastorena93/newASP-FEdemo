e71a827403bbfa2c3446549fd1af04780d199614
Mcastorena93 <mauriciocastorena29@gmail.com>
2023-08-21 15:54:38 -0400

    controllers update + models folder

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -2,9 +2,20 @@
 
 namespace CityInfo.API.Controllers
 {
+    [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
 
     {
-
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new{ id = 1, Name = "New York City" },
+                    new {id = 2, Name = "Antwerp"}
+                });
+        }
     }
 }
