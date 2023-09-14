0c9bd49f9faf390f4064e9118d56eeede4ecbbec
Mcastorena93 <mauriciocastorena29@gmail.com>
2023-09-14 15:19:46 -0400

    establish database

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -1,12 +1,25 @@
 ﻿using CityInfo.API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 namespace CityInfo.API.DbContexts
 {
     public class CityInfoContext : DbContext
     {
         public DbSet<City> Cities { get; set; } = null!;
 
-        public DbSet<PointOfInterest> PointOfInterests { get; set;} = null!;
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+          //  optionsBuilder.UseSqlite("Connectionstring");
+            //base.onConfiguring(OptionsBuilder);
+        //}
 
     }
 }
