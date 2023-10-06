53b771a2ec6423cca06066a292d13abb21040732
Mcastorena93 <mauriciocastorena29@gmail.com>
2023-10-06 06:18:52 -0400

    resource management : get/post

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -10,19 +10,25 @@ namespace CityInfo.API.Services
 
         public CityInfoRepository(CityInfoContext context)
         {
-            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context = context 
+                ?? throw new ArgumentNullException(nameof(context));
         }
         public async Task<IEnumerable<City>> GetCitiesAsync()
         {
-            return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
+            return await _context.Cities
+                .OrderBy(c => c.Name).ToListAsync();
         }
 
-        public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
+        public async Task<City?> GetCityAsync(
+            int cityId, 
+            bool includePointsOfInterest)
         {
             if (includePointsOfInterest)
             {
-                return await _context.Cities.Include(c => c.PointsOfInterest)
-                    .Where(c => c.Id == cityId).FirstOrDefaultAsync();
+                return await _context.Cities
+                    .Include(c => c.PointsOfInterest)
+                    .Where(c => c.Id == cityId)
+                    .FirstOrDefaultAsync();
             }
 
             return await _context.Cities
@@ -30,23 +36,43 @@ namespace CityInfo.API.Services
                 .FirstOrDefaultAsync();
         }
 
+        public async Task<bool> CityExistsAsync(
+            int cityId)
+        {
+            return await _context.Cities
+                .AnyAsync(c => c.Id == cityId);
+        }
+
         public async Task<PointOfInterest?> GetPointOfInterestForCityAsync(
-            int cityId, int pointOfInterestId)
+            int cityId, 
+            int pointOfInterestId)
         {
             return await _context.PointsOfInterest
                 .Where(p => p.CityId == cityId && p.Id == pointOfInterestId)
                 .FirstOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<PointOfInterest>> GetPointOfInterestForCityAsync(int cityId)
+        public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(
+            int cityId)
         {
             return await _context.PointsOfInterest
-                .Where(p => p.CityId == cityId).ToListAsync();
+                .Where(p => p.CityId == cityId)
+                .ToListAsync();
         }
 
-        public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId)
+        public async Task AddPointOfInterestForCityAsync(int cityId,
+            PointOfInterest pointOfInterest)
         {
-            return await _context.PointsOfInterest.Where(p => p.CityId == cityId).ToListAsync();
+            var city = await GetCityAsync(cityId, false);
+            if(city != null)
+            {
+                city.PointsOfInterest.Add(pointOfInterest);
+            }
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() >= 0);
         }
     }
 }
--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -9,8 +9,14 @@ namespace CityInfo.API.Services
 
         Task<City?> GetCityAsync(int cityId, bool includePointOfInterest);
 
-        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<bool> CityExistsAsync(int cityId);
 
         Task<PointOfInterest> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+
+        Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
+
+        Task<bool>  SaveChangesAsync();
     }
 }
